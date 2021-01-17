88ec49944d4767e374a6c0f3397bbbf5bf449896
aaanu <anu.gulati100@gmail.com>
2021-01-17 15:50:26 -0500

    init project

---
+++ resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,26 @@
+=begin
+Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+`resistor_color_duo_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-duo` directory.
+=end
+class ResistorColorDuo
+    @bands_map = {
+        'black' => 0,
+        'brown' => 1,
+        'red' => 2,
+        'orange' => 3,
+        'yellow' => 4,
+        'green' => 5,
+        'blue' => 6,
+        'violet' => 7,
+        'grey' => 8,
+        'white' => 9
+    }.freeze
+    def self.value(arr)
+        tens = @bands_map[arr[0]]
+        ones = @bands_map[arr[1]]
+        [tens,ones].join.to_i
+    end
+end
