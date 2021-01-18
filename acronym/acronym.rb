faaf55d415d0fff091cdd0d1a7a31c19de6c28af
aaanu <anu.gulati100@gmail.com>
2021-01-17 21:18:27 -0500

    add acronym

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -5,4 +5,8 @@ Write your code for the 'Acronym' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
-
+class Acronym
+    def self.abbreviate(jargon)
+        jargon.gsub(/[^a-zA-Z. ]/, "").split.map{|word| word[0]}.join.upcase
+    end
+end
